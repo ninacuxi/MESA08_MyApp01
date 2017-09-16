ba7f8fc928d9cd877b9e2d1aecd7be479ca01171
nina <ninacuxi@gmail.com>
2017-09-16 14:55:09 +0800

    20170915

--- MyApp01/ViewController.swift
+++ MyApp01/ViewController.swift
@@ -9,16 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    @IBAction func click(_ sender: Any) {
+        myLabel.text = createLottery()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showDialog(_ sender: Any) {
+        sDialog()
     }
+    
+    private func sDialog(){
+        let alert = UIAlertController(title: "Welcome", message: "恭喜老爺，賀喜夫人", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: {(a) -> Void in print("Press OK")})
+        alert.addAction(action)
+        present(alert, animated: true, completion: {
+            print("Show Dialog")
+        })     //completion: nil 也可寫成 completion: {}
+        
+    }
+    private func createLottery() -> String {
+        var set:Set<Int> = []
+        while set.count < 6 {
+            set.insert(Int(arc4random_uniform(49)+1))
+        }
+        
+        var ret = ""
+        for lottery in set {
+            ret += "\(lottery) "
+        }
+        return ret
+    }
+    
+
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
